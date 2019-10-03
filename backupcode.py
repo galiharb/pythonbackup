2e61373185b9279daea3ec7b27679ed332e88562
Galih Amar Barkah <galihfun03@gmail.com>
2019-10-03 09:50:29 +0700

    Update backupcode.py
    edit variable name and setting mysql backup

--- backupcode.py
+++ backupcode.py
@@ -1,29 +1,39 @@
 import os
 import datetime
 
-masuk = 'cd /home/surat/ &&'
+PATH ="/home/webfolder/" #without public_html
+TEMP ="/galih/tempwebapp/"
+BACKUP = "/backupAPP/"
+
+#### MYSQL #####
+USER =""
+PASSWORD =""
+DATABASE =""
+################
+
+masuk = 'cd '+PATH+' &&'
 
 perintah = 'tar -zcvf '
 
 today = datetime.date.today()
 yesterday = today - datetime.timedelta(days = 1)
 
-nama_file =  yesterday.strftime('%d-%b-%Y-esurat-code.tar.gz')
-nama_filedb = yesterday.strftime('%d-%b-%Y-esurat-db.sql')
-nama_backup = yesterday.strftime('%d-%b-%Y-backup-esurat.tar.gz')
+nama_file =  yesterday.strftime('%d-%b-%Y-app-code.tar.gz')
+nama_filedb = yesterday.strftime('%d-%b-%Y-app-db.sql')
+nama_backup = yesterday.strftime('%d-%b-%Y-backup-app.tar.gz')
 
 nama_folder = ' public_html'
 kompres = perintah+nama_file+nama_folder
 
 os.system(masuk+kompres)
-os.system('mv /home/surat/'+nama_file+' /root/galih/tempsurat/')
-os.system('mysqldump --single-transaction -usurat -p3B8wt0PFyOL1HyTX surat > /root/galih/tempsurat/'+nama_filedb)
+os.system('mv '+PATH+' '+nama_file+' '+TEMP)
+os.system('mysqldump --single-transaction -u'+USER+' -p'+PASSWORD+' '+DATABASE+' > 'TEMP+nama_filedb)
 
-os.system('cd /root/galih/tempsurat/ && tar -zcvf '+nama_backup+' '+nama_file+' '+nama_filedb)
-os.system('rm -f /root/galih/tempsurat/'+nama_file+' /root/galih/tempsurat/'+nama_filedb)
+os.system('cd '+TEMP+' && tar -zcvf '+nama_backup+' '+nama_file+' '+nama_filedb)
+os.system('rm -f 'TEMP+nama_file+' '+TEMP+nama_filedb)
 
-#pindah ke qnap
-os.system('mv /root/galih/tempsurat/'+nama_backup+' /backupEsurat/')
-os.system('rm -f /root/galih/tempsurat/'+nama_backup)
+#Move to backupfolder
+os.system('mv 'TEMP+nama_backup+' '+BACKUP)
+os.system('rm -f 'TEMP+nama_backup)
 
 print ("selesai")
